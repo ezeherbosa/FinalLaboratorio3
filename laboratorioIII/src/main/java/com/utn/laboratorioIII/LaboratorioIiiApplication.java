9fbdfa23aa8185dc9f57227245b629569b869ab1
Ezequiel Herbosa <89166192+ezeherbosa@users.noreply.github.com>
2024-09-10 22:57:52 -0300

    inicio proyecto

---
+++ laboratorioIII/src/main/java/com/utn/laboratorioIII/LaboratorioIiiApplication.java
@@ -0,0 +1,13 @@
+package com.utn.laboratorioIII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioIiiApplication.class, args);
+	}
+
+}
---
+++ laboratorioIII/src/test/java/com/utn/laboratorioIII/LaboratorioIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.laboratorioIII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
